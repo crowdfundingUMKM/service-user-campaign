8eddaf6c8548326d1549410dad5a23099be2788d
Zaky Lp <siahmadzaky@gmail.com>
2023-05-15 13:29:31 +0700

    update

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Service User Campaign")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// RUN SERVICE
+	// router := gin.Default()
+
+	// api := router.Group("api/v1")
+
 }
